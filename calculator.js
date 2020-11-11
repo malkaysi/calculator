3532f56afcdca4e104a7c7be6b530b3c6462c3f0
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-11-10 20:18:52 -0500

    operate

--- calculator.js
+++ calculator.js
@@ -7,3 +7,29 @@ function subtract (a, b) {
 	total = a - b;
 	return total;
 }
+
+function multiply(a, b){
+	total = a * b;
+	return total;
+}
+
+function divide(a, b){
+	total = a/b;
+	return total;
+}
+
+function operate(a, operator, b){
+	// Need to take an operator and 2 numbers then call a function
+
+	if(operator == '+'){
+		total = add(a, b);
+	}else if(operator == '-'){
+		total = subtract(a, b);
+	}else if(operator == '*'){
+		total = multiply(a, b);
+	}else if(operator == '/'){
+		total = divide(a, b);
+	}
+
+	return total;
+}
