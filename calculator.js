67d060dd2adee78162137b6b053159f137943465
malkaysi <58181661+malkaysi@users.noreply.github.com>
2020-11-14 21:54:41 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,33 +1,43 @@
 function add(a, b) {
 	total = a + b;
+	display.innerText = total;
 	return total;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
 	total = a - b;
+	display.innerText = total;
 	return total;
 }
 
-function multiply(a, b){
+function multiply(a, b) {
 	total = a * b;
+	display.innerText = total;
 	return total;
 }
 
-function divide(a, b){
-	total = a/b;
-	return total;
+function divide(a, b) {
+	if(b == 0){
+		alert('How dare you');
+		startOver();
+	} else {
+		total = a / b;
+		display.innerText = total;
+		return total;
+	}
+	
 }
 
-function operate(a, operator, b){
+function operate(a, operator, b) {
 	// Need to take an operator and 2 numbers then call a function
 
-	if(operator == '+'){
+	if (operator == '+') {
 		total = add(a, b);
-	}else if(operator == '-'){
+	} else if (operator == '-') {
 		total = subtract(a, b);
-	}else if(operator == '*'){
+	} else if (operator == 'X') {
 		total = multiply(a, b);
-	}else if(operator == '/'){
+	} else if (operator == '/') {
 		total = divide(a, b);
 	}
 
@@ -36,22 +46,55 @@ function operate(a, operator, b){
 
 const digits = document.querySelectorAll('.digit');
 const display = document.querySelector('.display');
+const operators = document.querySelectorAll('.operator');
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let calcOperator = "";
+
+display.innerText = '0';
 
 
 digits.forEach((digit) => {
-	digit.addEventListener('click', () => {
-		display.innerHTML += digit.innerHTML;		
+	digit.addEventListener('click', e => {
+
+		if (operator === "") {
+			firstNum += e.target.innerText;
+			display.innerText = firstNum;
+			firstNum = parseFloat(firstNum);
+		} else {
+			secondNum += e.target.innerText;
+			display.innerText = secondNum;
+			secondNum = parseFloat(secondNum);
+		}
+	})
+})
+
+operators.forEach((op) => {
+	op.addEventListener('click', e => {
+
+		if (e.target.innerText !== '=') {
+			operator = e.target.innerText;
+		} else {
+
+			operate(firstNum, operator, secondNum);
+		}
+
 	})
 })
 
 const clear = document.querySelector('.clear');
 
-clear.addEventListener('click', () => {
-	display.innerHTML = '';
+clear.addEventListener('click', e => {
+	startOver();
 })
 
-/*
-They are clicking on digits
-These digits are being passed to display.innertHTML
-When they click on an operator, a function should run, saving the value of the first pass to a variable
-*/
+function startOver(){
+	display.innerText = "0";
+	firstNum = '';
+	secondNum = '';
+	operator = '';
+	total = 0;
+}
+
